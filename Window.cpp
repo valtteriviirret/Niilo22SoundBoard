09b2501eb4b121a80546f1ca50cbc3a332fbe8f1
viirretvaltteri@gmail.com <viirretvaltteri@gmail.com>
2021-10-25 02:27:31 +0300

    window

---
+++ Window.cpp
@@ -0,0 +1,34 @@
+#include "Window.h"
+
+Window::Window() 
+{ 
+	createWindow(); 
+}
+
+Window::~Window()
+{
+	free();
+}
+
+bool Window::createWindow()
+{
+	bool w = true;
+	
+	// creating the window
+	window = SDL_CreateWindow("Niilo22 SoundBoard", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Screen::width, Screen::height, SDL_WINDOW_SHOWN);
+	if(window == NULL)
+	{
+		std::cout << "Could not create window! Error: " << SDL_GetError() << "\n";
+		w = false;
+	}
+	return w;
+}
+
+void Window::free()
+{
+	SDL_DestroyWindow(window);
+	window = NULL;
+}
+
+SDL_Window* Window::getWindow() { return window; }
+
