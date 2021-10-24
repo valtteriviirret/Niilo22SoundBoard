ecd65906ae67205cfbb36d6bf77438e77b2c4c1c
viirretvaltteri@gmail.com <viirretvaltteri@gmail.com>
2021-10-25 02:27:12 +0300

    renderer

---
+++ Renderer.cpp
@@ -0,0 +1,39 @@
+#include "Renderer.h"
+
+Renderer::Renderer()
+{
+	CreateRenderer();
+}
+
+Renderer::~Renderer()
+{
+	free();
+}
+
+
+bool Renderer::CreateRenderer()
+{
+	bool w = true;
+	
+	window = new Window;
+	// create the renderer
+	renderer = SDL_CreateRenderer(window->getWindow(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if(renderer == NULL)
+	{
+		std::cout << "Renderer could not be created! Error: " << SDL_GetError() << "\n";
+		w = false;
+	}
+	// initialize color
+	else
+		SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	return w;
+}
+
+void Renderer::free()
+{
+	SDL_DestroyRenderer(renderer);
+	renderer = NULL;
+}
+
+SDL_Renderer* Renderer::getRenderer() { return renderer; }
+
---
+++ Renderer.h
@@ -0,0 +1,22 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include "Window.h"
+#include <SDL2/SDL.h>
+#include <iostream>
+
+// this class creates the renderer
+class Renderer
+{
+	public:
+		Renderer();
+		~Renderer();
+		SDL_Renderer* getRenderer();
+	private:
+		SDL_Renderer* renderer;
+		void free();
+		bool CreateRenderer();
+		Window* window;
+};
+
+#endif
